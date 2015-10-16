0747d6819aeec288dff3dfc9842b839388b05fbf
Krunal Manohar Manik <krunal@uber.com>
2015-10-16 17:58:02 +0530

    Updated config

--- cabot/run.sh
+++ cabot/run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 export DATABASE_URL="postgres://docker:docker@db:5432/docker"
-export CELERY_BROKER_URL="redis://celery_broker:6379/1"
+export CELERY_BROKER_URL="redis://celery_broker:6394/1"
 export SES_HOST="smtp_server"
 export SES_USER=""
 export SES_PASS=""
 export SES_PORT="25"
+export GRAPHITE_API=http://grafana-graphite:8000
 
 python manage.py collectstatic --noinput &&\
 python manage.py compress --force &&\
