20cbdb79f54e4116690c336eb122f4fae4daef4b
Krunal Manohar Manik <krunal@uber.com>
2015-10-18 13:02:26 +0530

    exposed port 25 from smtp

--- cabot/run.sh
+++ cabot/run.sh
@@ -3,8 +3,6 @@
 export DATABASE_URL="postgres://docker:docker@db:5432/docker"
 export CELERY_BROKER_URL="redis://celery_broker:6394/1"
 export SES_HOST="smtp_server"
-export SES_USER=""
-export SES_PASS=""
 export SES_PORT="25"
 export GRAPHITE_API=http://grafana-graphite:8000/
 
